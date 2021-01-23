b8b60eda7a9171e046f4cc9d40d15b6997292cbd
Rene Valentin Gomez Hernandez <renegomez@MacBook-Pro-de-Rene.local>
2021-01-23 14:54:13 -0600

    iniciando proyeto

---
+++ index.php
@@ -0,0 +1,416 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<title>Resite | RVGH</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css?<?php echo time(); ?>">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css?<?php echo time(); ?>">
+
+	<!--Swiper js-->
+	<link rel="stylesheet" type="text/css" href="css/swiper-bundle.min.css">
+	<style type="text/css">
+		.swiper-container{
+			/*overflow: unset!important;*/
+		}
+		.swiper-container-horizontal>.swiper-pagination-bullets .swiper-pagination-bullet{
+			margin-bottom: -40px;
+		}
+		.swiper-container1 {
+      		width: 100%;
+      		height: 100%;
+      		overflow: hidden;
+    	}
+		.swiper-container2 {
+      		width: 100%;
+      		height: 100%;
+      		overflow: hidden;
+    	}
+	</style>
+</head>
+<body>
+	<section class="headerPage">
+		<div class="contenido">
+			<div class="fila">
+				<div class="column-12 column-m-12">
+					<div class="headerPage__contenido">
+						<img class="imagen imagen-centrada headerPage__contenido--logo" src="https://itedup.com/wp-content/uploads/itedup-logo-animated.gif">
+						<img class="imagen imagen-centrada headerPage__contenido--imagen" src="https://via.placeholder.com/1200x450.png">
+						<h1 class="molecula-titulo-principal color-secundario headerPage__contenido--titulo">Logotipos Profesionales</h1>
+						<h2 class="molecula-subtitulo color-secundarioA headerPage__contenido--subtitulo">Deja el diseño de tu marca en manos de los profesionales</h2>
+						<a class="boton background-primario color-blanco headerPage__contenido--boton" href="#paquetes">VER PLANES</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="paquetes" class="s-paquetes seccion-general" style="background-color: #f3f7f9;">
+		<div class="contenido">
+			<div class="fila">
+				<div class="column-12">
+					<h3 class="molecula-titulo-general color-secundario s-paquetes__titulo">Paquetes</h3>
+					<h4 class="molecula-subtitulos color-secundarioA s-paquetes__subtitulo">Escoge el paquete que necesites</h4>
+				</div>
+			</div>
+			<div class="fila">
+				<div class="column-12">
+					<!-- Swiper -->
+				  	<div class="swiper-container1">
+					    <div class="swiper-wrapper">
+					      	<div class="swiper-slide">
+						      	<div class="tarjeta s-paquetes__tarjeta">
+									<div class="tarjeta-header s-paquetes__tarjeta-header">
+										<h3 class="s-paquetes__tarjeta-header--nombre">Express</h3>
+										<h4 class="s-paquetes__tarjeta-header--precio">$350 <span class="s-paquetes__tarjeta-header--moneda">MXN</span> </h4>
+									</div>
+									<div class="tarjeta-body s-paquetes__tarjeta-body">
+										<ul class="s-paquetes__tarjeta-lista">
+											<li>1 Propuesta</li>
+								      		<li>3 Revisiones</li>
+										</ul>
+									</div>
+									<div class="tarjeta-footer s-paquetes__tarjeta-footer">
+										<a class="boton background-primario color-blanco b-w-d" href="https://web.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Express." target="_blank">LO QUIERO</a>
+										<a class="boton background-primario color-blanco b-w-m" href="https://api.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Express." target="_blank">LO QUIERO</a>
+									</div>
+								</div>
+					      	</div>
+					      	<div class="swiper-slide">
+					      		<div class="tarjeta s-paquetes__tarjeta">
+									<div class="tarjeta-header s-paquetes__tarjeta-header">
+										<h3 class="s-paquetes__tarjeta-header--nombre">Básico</h3>
+										<h4 class="s-paquetes__tarjeta-header--precio">$750 <span class="s-paquetes__tarjeta-header--moneda">MXN</span> </h4>
+									</div>
+									<div class="tarjeta-body s-paquetes__tarjeta-body">
+										<ul class="s-paquetes__tarjeta-lista">
+											<li>3 Propuestas</li>
+	      									<li>5 Revisiones</li>
+										</ul>
+									</div>
+									<div class="tarjeta-footer s-paquetes__tarjeta-footer">
+										<a class="boton background-primario color-blanco b-w-d" href="https://web.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Básico." target="_blank">LO QUIERO</a>
+										<a class="boton background-primario color-blanco b-w-m" href="https://api.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Básico." target="_blank">LO QUIERO</a>
+									</div>
+								</div>
+					      	</div>
+					      	<div class="swiper-slide">
+					      		<div class="tarjeta s-paquetes__tarjeta">
+									<div class="tarjeta-header s-paquetes__tarjeta-header">
+										<h3 class="s-paquetes__tarjeta-header--nombre">Avanzado</h3>
+										<h4 class="s-paquetes__tarjeta-header--precio">$1,050 <span class="s-paquetes__tarjeta-header--moneda">MXN</span> </h4>
+									</div>
+									<div class="tarjeta-body s-paquetes__tarjeta-body">
+										<ul class="s-paquetes__tarjeta-lista">
+											<li>5 Propuestas</li>
+					      					<li>5 Revisiones</li>
+										</ul>
+									</div>
+									<div class="tarjeta-footer s-paquetes__tarjeta-footer">
+										<a class="boton background-primario color-blanco b-w-d" href="https://web.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Avanzado." target="_blank">LO QUIERO</a>
+										<a class="boton background-primario color-blanco b-w-m" href="https://api.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Avanzado." target="_blank">LO QUIERO</a>
+									</div>
+								</div>
+					      	</div>
+					      	<div class="swiper-slide">
+					      		<div class="tarjeta s-paquetes__tarjeta">
+									<div class="tarjeta-header s-paquetes__tarjeta-header">
+										<h3 class="s-paquetes__tarjeta-header--nombre">Premium</h3>
+										<h4 class="s-paquetes__tarjeta-header--precio">$2,100 <span class="s-paquetes__tarjeta-header--moneda">MXN</span> </h4>
+									</div>
+									<div class="tarjeta-body s-paquetes__tarjeta-body">
+										<ul class="s-paquetes__tarjeta-lista">
+											<li>Estudio de Marca</li>
+					      					<li>3 Propuestas</li>
+					      					<li>5 Revisiones</li>
+										</ul>
+									</div>
+									<div class="tarjeta-footer s-paquetes__tarjeta-footer">
+										<a class="boton background-primario color-blanco b-w-d" href="https://web.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Premium." target="_blank">LO QUIERO</a>
+										<a class="boton background-primario color-blanco b-w-m" href="https://api.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Premium." target="_blank">LO QUIERO</a>
+									</div>
+								</div>
+					      	</div>
+					      	<div class="swiper-slide">
+					      		<div class="tarjeta s-paquetes__tarjeta">
+									<div class="tarjeta-header s-paquetes__tarjeta-header">
+										<h3 class="s-paquetes__tarjeta-header--nombre">Enterprise</h3>
+										<h4 class="s-paquetes__tarjeta-header--precio">$5,600 <span class="s-paquetes__tarjeta-header--moneda">MXN</span> </h4>
+									</div>
+									<div class="tarjeta-body s-paquetes__tarjeta-body">
+										<ul class="s-paquetes__tarjeta-lista">
+											<li>Estudio de Marca</li>
+					      					<li>5 Propuestas</li>
+					      					<li>5 Revisiones</li>
+					      					<li>Guía de Marca con 5 Slides</li>
+										</ul>
+									</div>
+									<div class="tarjeta-footer s-paquetes__tarjeta-footer">
+										<a class="boton background-primario color-blanco b-w-d" href="https://web.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Enterprise." target="_blank">LO QUIERO</a>
+										<a class="boton background-primario color-blanco b-w-m" href="https://api.whatsapp.com/send?phone=5215572000685&text=Hola%E2%80%9A%20estoy%20interesado%20en%20el%20paquetes%20Logotipo%20Enterprise." target="_blank">LO QUIERO</a>
+									</div>
+								</div>
+					      	</div>
+					    </div>
+					    <!-- Add Pagination -->
+    					<div class="swiper-pagination swiper-pagination-paquetes"></div>
+				  	</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="s-plan seccion-general">
+		<div class="contenido">
+			<div class="fila">
+				<div class="column-12">
+					<div class="s-plan__head">
+						<h3 class="molecula-titulo-general color-secundario s-plan__titulo">Plan de trabajo</h3>
+						<h4 class="molecula-subtitulos color-secundarioA s-plan__subtitulo">Escoge el paquete que necesites</h4>
+					</div>
+				</div>
+			</div>
+			<div class="fila">
+				<div class="column-12">
+					<!-- Tab links -->
+					<div class="tab">
+						<button class="tablinks" onclick="openCity(event, 'tabuno')" id="defaultOpen">1</button>
+  						<button class="tablinks" onclick="openCity(event, 'tabdos')">2</button>
+  						<button class="tablinks" onclick="openCity(event, 'tabtres')">3</button>
+					</div>
+					<hr class="linea-horizontal">
+
+					<!-- Tab content -->
+					<div id="tabuno" class="tabcontent">
+  						<div class="fila" style="align-items: center;">
+						    <div class="column-12 column-m-6">
+						    	<img class="imagen imagen-centrada slider__imagen" src="https://via.placeholder.com/400x200.png"/>
+						    </div>
+					    	<div class="column-12 column-m-6">
+					    		<h3 class="slider__titulo molecula-titulo-general color-secundario">1.- Inspiración</h3>
+					    		<p class="slider__descripcion color-secundarioA">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						    	<ul class="slider__ul">
+						    		<li class="slider__li">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+						    		<li class="slider__li">Excepteur sint occaecat cupidatat non proident.</li>
+						    		<li class="slider__li">Ut enim ad minim veniam, quis nostrud exercitation</li>
+						    	</ul>
+					    	</div>
+					    </div>
+					</div>
+
+					<div id="tabdos" class="tabcontent">
+  						<div class="fila" style="align-items: center;">
+						    <div class="column-12 column-m-6">
+						    	<img class="imagen imagen-centrada slider__imagen" src="https://via.placeholder.com/400x200.png"/>
+						    </div>
+					    	<div class="column-12 column-m-6">
+					    		<h3 class="slider__titulo molecula-titulo-general color-secundario">2.- Creatividad</h3>
+					    		<p class="slider__descripcion color-secundarioA">Reprehenderit in voluptate velit esse
+					    		cillum dolore eu fugiat nulla pariatur.</p>
+						    	<ul class="slider__ul">
+						    		<li class="slider__li">Excepteur sint occaecat cupidatat non proident.</li>
+						    		<li class="slider__li">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+						    		<li class="slider__li">Ut enim ad minim veniam, quis nostrud exercitation.</li>
+						    	</ul>
+					    	</div>
+					    </div>
+					</div>
+
+					<div id="tabtres" class="tabcontent">
+  						<div class="fila" style="align-items: center;">
+						    <div class="column-12 column-m-6">
+						    	<img class="imagen imagen-centrada slider__imagen" src="https://via.placeholder.com/400x200.png"/>
+						    </div>
+					    	<div class="column-12 column-m-6">
+					    		<h3 class="slider__titulo molecula-titulo-general color-secundario">3.- Profesionalismo</h3>
+					    		<p class="slider__descripcion color-secundarioA">Labore et dolore magna aliqua, ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p>
+						    	<ul class="slider__ul">
+						    		<li class="slider__li">Ut enim ad minim veniam, quis nostrud exercitation.</li>
+						    		<li class="slider__li">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+						    		<li class="slider__li">Excepteur sint occaecat cupidatat non proident.</li>
+						    	</ul>
+					    	</div>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="testimonios seccion-general">
+		<div class="contenido">
+			<div class="fila">
+				<div class="column-12">
+					<div class="testimonios__head">
+						<h3 class="molecula-titulo-general color-secundario testimonios__titulo">Testimonios</h3>
+						<h4 class="molecula-subtitulos color-secundarioA testimonios__subtitulo">Nuestro trabajo</h4>
+					</div>
+				</div>
+			</div>
+			<div class="fila">
+				<div class="column-12">
+					<!-- Swiper -->
+				  <div class="swiper-container2">
+				    <div class="swiper-wrapper">
+				      <div class="swiper-slide">
+				      	<div class="testimonios__body">
+				      		<img class="imagen testimonios__body--imagen" src="https://via.placeholder.com/100x50.png">
+				      		<h3 class="testimonios__body--nombreEmpresa">TXTint</h3>
+				      		<p class="testimonios__body--texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				      		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				      		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				      		consequat.</p>
+				      		<h4 class="testimonios__body--nombre">Rodrigo Martinez</h4>
+				      		<h5 class="testimonios__body--puesto">CEO</h5>
+				      	</div>
+				      </div>
+				      <div class="swiper-slide">
+				      	<div class="testimonios__body">
+				      		<img class="imagen testimonios__body--imagen" src="https://via.placeholder.com/100x50.png">
+				      		<h3 class="testimonios__body--nombreEmpresa">Cinecotiza</h3>
+				      		<p class="testimonios__body--texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				      		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				      		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				      		consequat.</p>
+				      		<h4 class="testimonios__body--nombre">Gerardo Mendez</h4>
+				      		<h5 class="testimonios__body--puesto">CEO</h5>
+				      	</div>
+				      </div>
+				      <div class="swiper-slide">
+				      	<div class="testimonios__body">
+				      		<img class="imagen testimonios__body--imagen" src="https://via.placeholder.com/100x50.png">
+				      		<h3 class="testimonios__body--nombreEmpresa">Snorback</h3>
+				      		<p class="testimonios__body--texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				      		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				      		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				      		consequat.</p>
+				      		<h4 class="testimonios__body--nombre">Victor Vizcaino</h4>
+				      		<h5 class="testimonios__body--puesto">CEO</h5>
+				      	</div>
+				      </div>
+				      <div class="swiper-slide">
+				      	<div class="testimonios__body">
+				      		<img class="imagen testimonios__body--imagen" src="https://via.placeholder.com/100x50.png">
+				      		<h3 class="testimonios__body--nombreEmpresa">Start Up - México</h3>
+				      		<p class="testimonios__body--texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				      		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				      		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				      		consequat.</p>
+				      		<h4 class="testimonios__body--nombre">Angel Saturo</h4>
+				      		<h5 class="testimonios__body--puesto">Co-Fundador</h5>
+				      	</div>
+				      </div>
+				    </div>
+				    <!-- Add Pagination -->
+    				<div class="swiper-pagination swiper-pagination-testimonios"></div>
+				    <!-- Add Arrows -->
+				    <div class="swiper-button-next testimonios__flecha-next"></div>
+				    <div class="swiper-button-prev testimonios__flecha-prev"></div>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="s-footer seccion-general" style="background-color: #f3f7f9;">
+		<div class="contenido">
+			<div class="fila">
+				<div class="column-12">
+					<img class="imagen s-footer__imagen" src="https://itedup.com/wp-content/uploads/itedup-it.svg">
+					<p class="s-footer__copy">2021 - <a class="s-footer__copy--link" href="#">Aviso Legal</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<!--Swiper js-->
+	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+	<!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper('.swiper-container1', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination-paquetes',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 10,
+        },
+      }
+    });
+  </script>
+  <script>
+  	$('#indicador li a').on('click', function(){
+    $('li a.activo').removeClass('activo');
+    $(this).addClass('activo');
+});
+  </script>
+
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper('.swiper-container2', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination-testimonios',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+      }
+    });
+  </script>
+  <script>
+		function openCity(evt, cityName) {
+		  // Declare all variables
+		  var i, tabcontent, tablinks;
+
+		  // Get all elements with class="tabcontent" and hide them
+		  tabcontent = document.getElementsByClassName("tabcontent");
+		  for (i = 0; i < tabcontent.length; i++) {
+		    tabcontent[i].style.display = "none";
+		  }
+
+		  // Get all elements with class="tablinks" and remove the class "active"
+		  tablinks = document.getElementsByClassName("tablinks");
+		  for (i = 0; i < tablinks.length; i++) {
+		    tablinks[i].className = tablinks[i].className.replace(" active", "");
+		  }
+
+		  // Show the current tab, and add an "active" class to the button that opened the tab
+		  document.getElementById(cityName).style.display = "block";
+		  evt.currentTarget.className += " active";
+		}
+	</script>
+	<script>
+		// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+	</script>
+</body>
+</html>
